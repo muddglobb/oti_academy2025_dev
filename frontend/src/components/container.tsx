d8fabb12f304354b20f2a617483c47a3172ebfa9
ayashar <187853299+ayashar@users.noreply.github.com>
2025-04-16 03:45:39 +0700

    feat(/components/container): added container component

--- frontend/src/components/container.tsx
+++ frontend/src/components/container.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+
 const Container = ({
     className,
     children,
@@ -6,10 +8,13 @@ const Container = ({
     children: React.ReactNode
 }) => {
     return (
-        <div
-            className={`container mx-auto px-4 sm:px-6 lg:px-8 ${className}`}
-        >
-            {children}
-        </div>
+        <main 
+            className={cn(
+                "mx-auto flex w-full max-w-(--breakpoints-2xl) flex-col gap-4 px-4 py-4 sm:px-8",
+                className)}>
+                    {children}
+        </main>
     )
 };
+
+export default Container
