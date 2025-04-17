dfe785424ef5300039df042df49cc8fb56dda449
ayashar <187853299+ayashar@users.noreply.github.com>
2025-04-17 21:54:17 +0700

    feat(components/logo): added logo component to the navbar

--- frontend/src/components/home/logo.tsx
+++ frontend/src/components/home/logo.tsx
@@ -1,17 +1,15 @@
 import Image from "next/image";
-import Link from "next/link";
 
 const Logo = () => {
     return (
-        <Link href="/" className="flex items-center">
         <Image
-            src="" //belom ada logonya, belom ada directorynya juga
+            src="/logo-placeholder.jpeg" 
             alt="Logo"
             width={40}
             height={40}
             className="mr-2"
         />
-        <span className="text-2xl font-bold">MyApp</span>
-        </Link>
     );
 }
+
+export default Logo;   
