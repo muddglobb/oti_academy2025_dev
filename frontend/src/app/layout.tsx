f43eebae52bc06bfe2be7b5b674f1e650efac612
muddglobb <dhimassulistio2@gmail.com>
2025-04-21 20:21:48 +0700

    feat(footer): add footer.tsx to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/home/navbar";
+import Footer from "@/components/home/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         <Navbar></Navbar>
         <main>{children}</main>
+        <Footer></Footer>
       </body>
     </html>
   );
