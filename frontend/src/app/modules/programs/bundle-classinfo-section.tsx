185251abfe9654fccdd209ded95c0be9a8eaf59c
Gigvo <giganiusrevo@gmail.com>
2025-04-23 13:11:27 +0700

    fix(background): fix background entry and bundle, kurang responsive

--- frontend/src/app/modules/programs/bundle-classinfo-section.tsx
+++ frontend/src/app/modules/programs/bundle-classinfo-section.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/legacy/image";
 
-const EntryClassInfo = () => {
+const BundleClassInfo = () => {
   return (
     <>
       <div className="relative w-full overflow-hidden">
@@ -13,7 +13,7 @@ const EntryClassInfo = () => {
           <p className="text-[2rem] text-center font-bold bg-gradient-to-b from-gray-300 to-gray-400 bg-clip-text text-transparent">
             Detailed Information About Our Class
           </p>
-          <div className="w-4xl">
+          <div className="w-4xl flex flex-col justify-center items-center gap-[25px]">
             <div className="flex flex-row justify-center gap-x-8">
               <div className="flex flex-col w-74 h-108 bg-white rounded-lg">
                 <div className="w-[297px] h-[284px] bg-[url(/person-placeholder.jpeg)] rounded-lg mask-b-from-90% bg-cover">
@@ -44,43 +44,72 @@ const EntryClassInfo = () => {
                   ratione voluptates dignissimos!
                 </p>
               </div>
-              <div className="text-sm text-white flex flex-col gap-2">
-                <div className="flex flex-row gap-2.5 border-s-white border-2 rounded-lg w-sm px-2.5 py-2">
-                  <Image
-                    src={"/icons/calendar-icon.svg"}
-                    alt="calendar-icon"
-                    width={21}
-                    height={21}
-                  ></Image>
-                  <p>1 - 15 June 2025</p>
+              <div className="flex flex-col w-74 h-108 bg-white rounded-lg">
+                <div className="w-[297px] h-[284px] bg-[url(/person-placeholder.jpeg)] rounded-lg mask-b-from-90% bg-cover">
+                  <div className="flex flex-row justify-between m-5">
+                    <div className="text-white flex flex-col gap-1 ">
+                      <div className="bg-primary-800 rounded-[5px] font-bold text-center h-auto w-[162px]">
+                        <p className="text-[22px] ">Dhimas Putra</p>
+                      </div>
+                      <div className="bg-primary-800 rounded-[5px] w-[122px] text-center">
+                        <p className="text-[12px]">Teaching Assistant</p>
+                      </div>
+                    </div>
+                    <button className="w-[30px] h-[30px] ">
+                      <Image
+                        src={"/icons/linkedin-icon.svg"}
+                        alt="linkedin-icon"
+                        width={30}
+                        height={30}
+                        className="bg-primary-50 rounded-[5px] mt-0 p-[5px] border-s-white border-[1px] "
+                      ></Image>
+                    </button>
+                  </div>
                 </div>
-                <div className="flex flex-row gap-2.5 border-s-white border-2 rounded-lg w-sm px-2.5 py-2">
-                  <Image
-                    src={"/icons/target-icon.svg"}
-                    alt="calendar-icon"
-                    width={21}
-                    height={21}
-                  ></Image>
-                  <p>6 Sesi</p>
-                </div>
-                <div className="flex flex-row gap-2.5 border-s-white border-2 rounded-lg w-sm px-2.5 py-2">
-                  <Image
-                    src={"/icons/time-icon.svg"}
-                    alt="calendar-icon"
-                    width={21}
-                    height={21}
-                  ></Image>
-                  <p>2 Jam/Sesi</p>
-                </div>
-                <div className="flex flex-row gap-2.5 border-s-white border-2 rounded-lg w-sm px-2.5 py-2">
-                  <Image
-                    src={"/icons/stack-icon.svg"}
-                    alt="calendar-icon"
-                    width={21}
-                    height={21}
-                  ></Image>
-                  <p>10 Modul</p>
-                </div>
+                <p className="m-4 text-xs">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolore, fuga rem quibusdam nulla commodi excepturi fugit vitae
+                  sint quia distinctio id. Ipsa a dolorem inventore quas aliquid
+                  ratione voluptates dignissimos!
+                </p>
+              </div>
+            </div>
+            <div className="text-sm text-white flex flex-col gap-2">
+              <div className="flex flex-row gap-2.5 border-s-white border-2 rounded-lg w-[619px] px-2.5 py-2">
+                <Image
+                  src={"/icons/calendar-icon.svg"}
+                  alt="calendar-icon"
+                  width={21}
+                  height={21}
+                ></Image>
+                <p>1 - 15 June 2025</p>
+              </div>
+              <div className="flex flex-row gap-2.5 border-s-white border-2 rounded-lg w-[619px] px-2.5 py-2">
+                <Image
+                  src={"/icons/target-icon.svg"}
+                  alt="calendar-icon"
+                  width={21}
+                  height={21}
+                ></Image>
+                <p>6 Sesi</p>
+              </div>
+              <div className="flex flex-row gap-2.5 border-s-white border-2 rounded-lg w-[619px] px-2.5 py-2">
+                <Image
+                  src={"/icons/time-icon.svg"}
+                  alt="calendar-icon"
+                  width={21}
+                  height={21}
+                ></Image>
+                <p>2 Jam/Sesi</p>
+              </div>
+              <div className="flex flex-row gap-2.5 border-s-white border-2 rounded-lg w-[619px] px-2.5 py-2">
+                <Image
+                  src={"/icons/stack-icon.svg"}
+                  alt="calendar-icon"
+                  width={21}
+                  height={21}
+                ></Image>
+                <p>10 Modul</p>
               </div>
             </div>
           </div>
@@ -242,4 +271,4 @@ const EntryClassInfo = () => {
   );
 };
 
-export default EntryClassInfo;
+export default BundleClassInfo;
