fc0895f3d0adeeff75a7eaceb409d7a1d9aca6e6
Gigvo <giganiusrevo@gmail.com>
2025-05-31 22:45:29 +0700

    fix(class-dashboard/[id]): fix page sizing

--- frontend/src/components/dashboard/video-teaser.tsx
+++ frontend/src/components/dashboard/video-teaser.tsx
@@ -4,7 +4,7 @@ import VideoPlayer from "./video-player";
 const VideoTeaser = () => {
   return (
     <div className="w-full border-sol border-2 border-neutral-500 rounded-[20px] px-2 py-5 md:p-5 flex flex-col">
-      <p className="font-bold text-lg text-neutral-50 border-b-2 border-neutral-500 pb-2">
+      <p className="font-bold text-lg text-neutral-50 border-b-2 border-neutral-500 pb-2 px-4">
         Video Teaser
       </p>
       <VideoPlayer
