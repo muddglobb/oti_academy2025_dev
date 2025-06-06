e0908ce53015d0ba14d63dff6a7b2f9a09aa25f5
muddglobb <dhimassulistio2@gmail.com>
2025-06-06 15:43:44 +0700

    fix(logout): lafggiiiiiii

--- frontend/src/app/api/logout/route.ts
+++ frontend/src/app/api/logout/route.ts
@@ -2,38 +2,33 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(_req: NextRequest) {
-  void _req;
+  // void _req;
   try {
     const cookieStore = await cookies();
     const accessToken = cookieStore.get("access_token")?.value;
     const refreshToken = cookieStore.get("refresh_token")?.value;
 
-    
-    // if (refreshToken || accessToken) {
-      try {
-        await fetch(`${process.env.AUTH_URL}/auth/logout`, {
-          method: "POST",
-          credentials: "include",
-          headers: {
-            "Content-Type": "application/json",
-            ...(accessToken && {
-              Authorization: `Bearer ${accessToken}`,
-            }),
-            // ...(refreshToken && {
-            //   Cookie: `refresh_token=${refreshToken}`,
-            // }),
-          },
-          body: JSON.stringify({
-            ...(refreshToken && { refreshToken }),
-          }),
-        });
-      } catch (err) {
-        console.error("Gagal logout ke backend:", err);
-      }
-    // }
-    
     cookieStore.delete("access_token");
     cookieStore.delete("refresh_token");
+
+    // if (refreshToken || accessToken) {
+    try {
+      await fetch(`${process.env.AUTH_URL}/auth/logout`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          // "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+          // ...(refreshToken && {
+          //   Cookie: `refresh_token=${refreshToken}`,
+          // }),
+        },
+        body: JSON.stringify({ refreshToken }),
+      });
+    } catch (err) {
+      console.error("Gagal logout ke backend:", err);
+    }
+    // }
     // alert("berhasil logout");
     return NextResponse.json({ message: "Logout success" });
   } catch (err) {
