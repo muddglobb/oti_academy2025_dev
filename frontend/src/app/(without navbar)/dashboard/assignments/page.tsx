63bdf348669f753f5c85d8617e2fae746508d7fd
muddglobb <dhimassulistio2@gmail.com>
2025-06-08 08:01:41 +0700

    feat: add temp page

--- frontend/src/app/(without navbar)/dashboard/assignments/page.tsx
+++ frontend/src/app/(without navbar)/dashboard/assignments/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { Lock } from "lucide-react";
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-full mt-40 gap-2">
+      <div className="flex gap-3 items-center">
+        <Lock className=""/>
+        <p className="font-bold text-2xl">Halaman Ini Masih Terkunci</p>
+      </div>
+      <p>
+        Kamu belum bisa mengakses konten ini. Pastikan kamu sudah menyelesaikan
+        langkah atau tugas sebelumnya, ya!
+      </p>
+    </div>
+  );
+};
 
-export default page
+export default page;
