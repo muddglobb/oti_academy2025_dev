f5d7e907acdb3df032b71dcc801e8db41a58754a
muddglobb <dhimassulistio2@gmail.com>
2025-06-14 12:02:10 +0700

    fix: clean sidebar.tsx code

--- frontend/src/components/dashboard/sidebar.tsx
+++ frontend/src/components/dashboard/sidebar.tsx
@@ -13,21 +13,12 @@ const Sidebar = () => {
 
 const handleLogout = async () => {
   try {
-    // const res = await fetch("/api/logout", {
-    //   method: "POST",
-    //   credentials: "include", 
-    //   // headers: {
-    //   //   "Content-Type": "application/json"
-    //   // }
-    // });
-
       const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/logout`, {
         method: 'POST',
-        credentials: 'include', // Ensure cookies are sent
+        credentials: 'include',
       })
     
     if (res.ok) {
-      // alert("berhasil logout");
       router.push("/");
     } else {
       const errorData = await res.json();
@@ -37,35 +28,10 @@ const handleLogout = async () => {
     console.error("Error saat logout:", err);
   }
 };
-
-// const handleLogout = async () => {
-//   const toastId = toast.loading('Memproses logout...')
-//     try {
-//       const res = await fetch(`${process.env.NEXT_PUBLIC_AUTH_URL}/auth/logout`, {
-//         method: 'POST',
-//         credentials: 'include', // Ensure cookies are sent
-//       })
-  
-//       if (!res.ok) {
-//         throw new Error('Failed to logout from the backend.')
-//       }
-  
-//       toast.dismiss(toastId)
-//       toast.success('Berhasil keluar dari akun.')
-//       router.push('/')
-//       router.refresh()
-//     } catch (error) {
-//       toast.dismiss(toastId)
-//       console.error('Logout error: ', error)
-//       toast.error('Gagal keluar dari akun. Silahkan coba lagi.')
-//     }
-// }
   return (
     <div className="w-62 bg-gray-950 flex flex-col border-r-2 border-neutral-500 fixed top-0 left-0 h-screen items-center">
       <Link href={"/"}>
         <div className="pt-6.5">
-          {/* <h1 className="text-xl font-bold">OmahTI</h1>
-          <p className="text-sm text-gray-400">ACADEMY</p> */}
           <Image
             src={"/images/logo/oti-academy.webp"}
             width={150}
