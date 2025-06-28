7cf864ea91e17454a6bdd28815c9eed33507eac7
muddglobb <dhimassulistio2@gmail.com>
2025-06-28 22:45:00 +0700

    feat(dashboard): calling card to group payment

--- frontend/src/app/(without navbar)/dashboard/page.tsx
+++ frontend/src/app/(without navbar)/dashboard/page.tsx
@@ -4,12 +4,14 @@ import WelcomeCard from "@/components/dashboard/welcome-card";
 import SecondContent from "@/components/dashboard/second-content";
 import SelectClass from "@/components/dashboard/select-class";
 import { getUsers } from "@/lib/auth/fetch-users";
+import AjakTeman from "@/components/dashboard/ajak-teman";
 
 const Dashboard = async () => {
   const users = await getUsers();
   return (
     <main className="px-2 py-2 sm:px-6 sm:py-4 md:px-10 lg:px-14 md:py-6 lg:py-8 bg-neutral-900">
       <WelcomeCard userName={users.data.name} Phone={users.data.phone}/>
+      <AjakTeman />
       <SecondContent/>
       <SelectClass />
     </main>
---
+++ frontend/src/components/dashboard/ajak-teman.tsx
@@ -0,0 +1,37 @@
+import { ArrowUpRight } from "lucide-react";
+import React from "react";
+import Link from "next/link";
+
+const AjakTeman = () => {
+  return (
+    <div className="bg-primary-800 rounded-[20px] p-5 mb-6 border-2 border-neutral-500 flex flex-col gap-3">
+      <p className="text-[22px] font-bold">
+        Ajak Teman, Dapat Potongan{" "}
+        <span className="text-error-300">Rp10.000</span>!
+      </p>
+      <p className="text-justify">
+        Sekarang beli kelas intermediate bisa <strong>lebih hemat</strong>! Ajak
+        teman-temanmu dan dapatkan potongan{" "}
+        <span className="text-error-300 font-bold">Rp10.000 per orang.</span>{" "}
+        Cukup beli{" "}
+        <span className="text-error-300 font-bold">
+          minimal mengajak 1 teman,
+        </span>{" "}
+        kalian
+        <strong>bebas pilih</strong>{" "}
+        <span className="text-error-300 font-bold">satu topik</span>{" "}
+        <strong>yang berbeda</strong> sesuai minat masing-masing.
+      </p>
+      <div className="self-end">
+        <Link href={"/"}>
+          <button className="flex items-center gap-2 bg-primary-400 px-3 py-2 rounded-[8px] cursor-pointer">
+            <p>Beli Sekarang</p>
+            <ArrowUpRight />
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AjakTeman;
